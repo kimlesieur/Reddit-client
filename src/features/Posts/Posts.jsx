3463273c4e15e67bf9c70c4b475e0b8cc3c04cbb
unknown <lesieurkim@gmail.com>
2022-05-11 19:21:03 +0200

    heroku deployment

--- src/features/Posts/Posts.jsx
+++ src/features/Posts/Posts.jsx
@@ -1,10 +1,13 @@
 import './Posts.css';
 import Post from '../../components/Post/Post';
+import Comments from '../../features/Comments/Comments';
+
 import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {selectPosts, selectSubreddit, isError, loadPosts, isLoading} from './postsSlice';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
 
 
 const Posts = () => {
@@ -24,16 +27,22 @@ const Posts = () => {
       }
 
 return ( 
-    <Container sx={{ py: 8 }} maxWidth="md">
-        <Grid container spacing={2}>
+    <Container sx={{ py: 2 }} maxWidth="md">
+        
             {Object.values(posts).map((post, index) => {
-                return <Post
-                    key={post.id}
-                    post={post}
-                    index={index}
-                />
+                return <Card sx={{ height: '100%', m:2 }} >
+                    <Grid container sx={{}}>
+                        <Post
+                        key={post.id}
+                        post={post}
+                        index={index}
+                        />
+                        <Comments numComments={post.num_comments} postId={post.id} postName={post.name} permalink={post.permalink} showingComments={post.showingComments} index={index} />
+                    </Grid>
+                </Card>
+                    
             })}
-        </Grid>
+            
     </Container>
     );
 };
